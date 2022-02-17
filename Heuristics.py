142514f75ea600e15c927876fccb8721d108c7d5
Juan Daniel <danielalejandre1050@gmail.com>
2022-02-17 14:19:59 -0600

    Trying to enter into a instance file

--- Heuristics.py
+++ Heuristics.py
@@ -1,6 +1,7 @@
-
-
 # Menu Function for print the menu of choices to solve the KP instance
+import os
+
+
 def menu():
     menu_log = """
     Knapsack problem Heuristic menu
@@ -18,6 +19,34 @@ def menu():
 
 # Function for create a switch case option to choose the heuristic
 def heuristicSwitch(option):
+    # This is the path where Python searches for instances
+    path = "instances/"
+
+    # Create a list to store a files in a list
+    is_file = os.path.isfile(path)
+
+    dir = os.listdir(path)
+
+    # Loop to print all the instances available
+    for file in range(len(dir)):
+        print(dir[file])
+
+    file_option = int(input("Which file would you like to open?: "))
+
+    check = os.path.isfile("instances/output_" + str(file_option) + ".txt")
+    print(check)
+
+    try:
+        with open("instances/output_" + str(file_option) + ".txt") as instance:
+            content = instance.read()
+            print(content)
+    except IOError:
+        print("Can't access to the file")
+
+    while not check:
+        menu()
+
+
     switcher = {
         1: heuristicOne,
         2: heuristicTwo,
@@ -40,6 +69,13 @@ def heuristicTwo():
 
 def heuristicThree():
     print("Heuristic 3")
+
+
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
 
 
 if __name__ == '__main__':
