8e3a073638f7f0bcf9c4bc631e8bc7b19414921f
Juan Daniel <danielalejandre1050@gmail.com>
2022-02-17 16:11:39 -0600

    Merge Heuristic main

--- Heuristics.py
+++ Heuristics.py
@@ -1,10 +1,23 @@
 # Menu Function for print the menu of choices to solve the KP instance
 import os
-import sys
 from pathlib import Path
 
+def heuristicMenu():
+    menu_log = """
+        Knapsack problem Heuristic menu
+        Choose which heuristic want to use to resolve the KP
 
-def instanceReader():
+        1. Heuristic (1)
+        2. Heuristic (2)
+        3. Heuristic (3)
+
+        """
+    print(menu_log)
+    option = int(input("Option: "))
+    instanceReader(option)
+
+
+def instanceReader(option):
 
     dir_path_file = "instances/"
     dir_path = os.listdir(dir_path_file)
@@ -32,7 +45,23 @@ def instanceReader():
         print(f'The file {path_to_file} exist')
     else:
         print(f'The file {path_to_file} does not exist')
-        instanceReader()
+        heuristicMenu()
+
+    if option == 1:
+        heuristicOne()
+    elif option == 2:
+        heuristicTwo()
+    elif option == 3:
+        heuristicThree()
+    else:
+        print("Option does not exist")
+
+def heuristicOne():
+    print("Heuristic 1")
+def heuristicTwo():
+    print("Heuristic 2")
+def heuristicThree():
+    print("Heuristic 3")
 
 if __name__ == '__main__':
-    instanceReader()
+    heuristicMenu()
